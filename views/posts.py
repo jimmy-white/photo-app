4607a04afea3ffe40e9ece05738809f4a33ad3f3
James White <jameswhite2022@u.northwestern.edu>
2022-05-01 19:41:46 -0500

    lab05

--- views/posts.py
+++ views/posts.py
@@ -1,6 +1,6 @@
 from flask import Response, request
 from flask_restful import Resource
-from models import Post, db
+from models import Post, db, Following
 from views import get_authorized_user_ids
 
 import json
@@ -14,15 +14,41 @@ class PostListEndpoint(Resource):
         self.current_user = current_user
 
     def get(self):
-        # get posts created by one of these users:
-        # print(get_authorized_user_ids(self.current_user))
-        return Response(json.dumps([]), mimetype="application/json", status=200)
+        userAndFriendsIds = get_authorized_user_ids(self.current_user)
+        posts = Post.query.filter(Post.user_id.in_(userAndFriendsIds))
+        limit = request.args.get('limit')
+
+        if limit: 
+            try: 
+                limit = int(limit)
+            except: 
+                return Response(json.dumps({'message': 'invalid limit'}), mimetype="application/json", status=400)
+
+            if limit > 50 or limit < 1: 
+                return Response(json.dumps({'message': 'number of posts must be between 1 and 50'}), mimetype="application/json", status=400)
+        else: 
+            limit = 20
+
+        posts = posts.order_by(Post.pub_date.desc()).limit(limit)
+        data = [item.to_dict() for item in posts.all()]
+
+        return Response(json.dumps(data), mimetype="application/json", status=200)
 
     def post(self):
-        # create a new post based on the data posted in the body 
         body = request.get_json()
-        print(body)  
-        return Response(json.dumps({}), mimetype="application/json", status=201)
+        
+        if not body: 
+            return Response(json.dumps({'message': 'bad data'}), mimetype="application/json", status=400)
+
+        image_url = body.get('image_url')
+        caption = body.get('caption')
+        alt_text = body.get('alt_text')
+        user_id = self.current_user.id
+        post = Post(image_url, user_id, caption, alt_text)
+
+        db.session.add(post)
+        db.session.commit()
+        return Response(json.dumps(post.to_dict()), mimetype="application/json", status=201)
         
 class PostDetailEndpoint(Resource):
 
@@ -31,20 +57,50 @@ class PostDetailEndpoint(Resource):
         
 
     def patch(self, id):
-        # update post based on the data posted in the body 
+        if not str(id).isdigit():
+            return Response(json.dumps({'message': 'Invalid ID '}), mimetype="application/json", status=400)
+
+        post = Post.query.get(id)
+
+        if not post or post.user_id != self.current_user.id:
+            return Response(json.dumps({'message': 'Post does not exist'}), mimetype="application/json", status=404)
+       
         body = request.get_json()
-        print(body)       
-        return Response(json.dumps({}), mimetype="application/json", status=200)
+        post.image_url = body.get('image_url') or post.image_url
+        post.caption = body.get('caption') or post.caption
+        post.alt_text = body.get('alt_text') or post.alt_text
+        
+        db.session.commit()        
+        return Response(json.dumps(post.to_dict()), mimetype="application/json", status=200)
 
 
     def delete(self, id):
-        # delete post where "id"=id
-        return Response(json.dumps({}), mimetype="application/json", status=200)
+        if not str(id).isdigit(): 
+            return Response(json.dumps({'message': 'invalid Id '}), mimetype="application/json", status=400)
+        
+        post = Post.query.get(id)
+        if not post or post.user_id != self.current_user.id:
+            return Response(json.dumps({'message': 'Post does not exist'}), mimetype="application/json", status=404)
+       
+
+        Post.query.filter_by(id=id).delete()
+        db.session.commit()
+        serialized_data = {
+            'message': 'Post {0} successfully deleted.'.format(id)
+        }
+        return Response(json.dumps(serialized_data), mimetype="application/json", status=200)
 
 
     def get(self, id):
-        # get the post based on the id
-        return Response(json.dumps({}), mimetype="application/json", status=200)
+        if not str(id).isdigit():
+            return Response(json.dumps({'message': 'invalid ID'}), mimetype="application/json", status=400)
+        
+        post = Post.query.get(id)
+
+        if not post or post.user_id not in get_authorized_user_ids(self.current_user):
+            return Response(json.dumps({'message': 'Post does not exist'}), mimetype="application/json", status=404)
+        
+        return Response(json.dumps(post.to_dict()), mimetype="application/json", status=200)
 
 def initialize_routes(api):
     api.add_resource(
